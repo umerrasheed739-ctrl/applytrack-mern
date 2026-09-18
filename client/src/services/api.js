0f82946cf4f52ccd5ae6853d9f57bd01326eed87
Umer Rasheed <umerrasheed739@gmail.com>
2026-09-19 03:05:36 +0500

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://server-rho-ten-50.vercel.app/api",
 });
 
 api.interceptors.request.use(
